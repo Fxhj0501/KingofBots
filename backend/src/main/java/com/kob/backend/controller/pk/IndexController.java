612b8e2a45e52ecb13f76bdec4b2da6d69242cf5
Fxhj0501 <3037989509@qq.com>
2023-02-17 17:51:00 -0500

    something wrong on get data from database

--- backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
@@ -1,6 +1,9 @@
 package com.kob.backend.controller.pk;
 
+import com.kob.backend.pojo.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
--- backend/src/main/java/com/kob/backend/controller/user/UserController.java
+++ backend/src/main/java/com/kob/backend/controller/user/UserController.java
@@ -1,9 +1,11 @@
 package com.kob.backend.controller.user;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.kob.backend.mapper.UserMapper;
 import com.kob.backend.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,5 +17,24 @@ public class UserController {
     @GetMapping("user/all/")
     public List<User> getAll(){
         return userMapper.selectList(null);
+    }
+    @GetMapping("/user/{userId}/")
+    public User getuser(@PathVariable int userId){
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("id",userId);
+        return userMapper.selectOne(queryWrapper);
+    }
+
+    @GetMapping("/user/add/{userId}/{username}/{password}/")
+    public String addUser(@PathVariable int userId,@PathVariable String username,@PathVariable String password){
+        User user= new User(userId,username,password);
+        userMapper.insert(user);
+        return "Add successfully";
+    }
+
+    @GetMapping("/user/delete/{userId}/")
+    public String deleteUser(@PathVariable int userId){
+        userMapper.deleteById(userId);
+        return "Delete successfully";
     }
 }
