8173145d029590c17edb092955f2f3b58a2bb343
Fxhj0501 <3037989509@qq.com>
2023-02-19 11:45:23 -0500

    finished login fore-end part

--- web/src/components/NavBar.vue
+++ web/src/components/NavBar.vue
@@ -14,17 +14,30 @@
             <router-link :class="route_name == 'ranklist_index'? 'nav-link active' : 'nav-link'" :to="{name:'ranklist_index'}">排行榜</router-link>
         </li>
       </ul>
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0" v-if="$store.state.user.is_login">
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Zijian Feng
+            {{$store.state.user.username}}
           </a>
           <ul class="dropdown-menu">
             <li><router-link class="dropdown-item" :to="{name:'user_bots_index'}">我的Bot</router-link></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">退出</a></li>
+            <li><a class="dropdown-item" href="#" @click="logout">退出</a></li>
           </ul>
         </li>
+      </ul>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0" v-else>
+        <li class="nav-item ">
+          <router-link class="nav-link" :to="{name:'user_account_login'}" role="button" >
+            登录
+          </router-link>
+        </li>
+          <li class="nav-item ">
+            <router-link :to="{name:'user_account_register'}" class="nav-link " href="#" role="button" >
+            注册
+            </router-link>
+          </li>
+          
       </ul>
     </div>
   </div>
@@ -33,12 +46,18 @@
 <script>
 import {useRoute} from 'vue-router';
 import { computed } from 'vue';
+import { useStore } from 'vuex';
 export default{
   setup(){
+    const store = useStore();
     const route = useRoute();
     let route_name = computed(()=>route.name);
+    const logout=()=>{
+      store.dispatch("logout");
+    }
     return {
-      route_name
+      route_name,
+      logout
     }
   }
 }
